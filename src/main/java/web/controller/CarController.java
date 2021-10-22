c39fef9e2d8247164c995cf821e132da6f67467e
Dmitriy Ilin <id1289@yandex.ru>
2021-10-22 22:43:54 +0300

    бизнес логика перенÑесена в сервис слой

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,19 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.models.Car;
 import web.service.CarService;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class CarController {
 
     @GetMapping("/cars")
     public String printSetOfCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
-        List<Car> cars = CarService.getCars().stream().limit(count).collect(Collectors.toList());
-        model.addAttribute("cars", cars);
+        model.addAttribute("cars", CarService.setOfCars(count));
         return "cars";
     }
 }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -4,16 +4,22 @@ import web.models.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarService {
 
-    public static List<Car> getCars(){
-        List<Car> cars = new ArrayList<>();
+    private static List<Car> cars = new ArrayList<>();
+
+    static {
         cars.add(new Car("BadCar", "Black", 1));
         cars.add(new Car("BetterCar", "White", 2));
         cars.add(new Car("GoodCar", "Orange", 3));
         cars.add(new Car("AwesomeCar", "Silver", 4));
         cars.add(new Car("SuperCar", "Red", 5));
-        return cars;
+    }
+
+
+    public static List<Car> setOfCars(int i){
+        return CarService.cars.stream().limit(i).collect(Collectors.toList());
     }
 }
